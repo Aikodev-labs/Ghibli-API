16966fff10037d8d399718c7325e7fdfd115d769
DESKTOP-79SKK4E\pedro <pedroizanuttini@gmail.com>
2023-06-22 07:39:23 +1000

    add delete and put routes

--- src/controllers/movies.controllers.js
+++ src/controllers/movies.controllers.js
@@ -3,7 +3,7 @@ const Movie = require('../models/movie.js');
 
 const createMovie = async (req, res = response) => {
     try {
-        const movie = Movie.create(req.body);
+        const movie = await Movie.create(req.body);
         return res.status(201).json({
             ok: true,
             movie
@@ -18,28 +18,51 @@ const createMovie = async (req, res = response) => {
 const getAllMovies = async (req, res = response) => {
     try {
         const movies = await Movie.find();
+        return res.status(200).json({
+            movies: movies,
+            status: "success"
+        })
     } catch (error) {
         return res.status(500).json({
             error: 'Internal server error'
         })
     }
-    // try{
-    //     const movies= await db.Movies.findAll();
-    //     return res.status(200).json({
-    //         ok:true,
-    //         movies
-    //     })
-    // } catch(error){
-    //     return res.status(500).json({
-    //         error:'Internal server error'
-    //     })
-    // }
+}
+
+const updateMovie = async(req,res=response) =>{
+    try {
+        const movies= await Movie.find();
+        const movie=await Movie.updateOne({
+            ...req.body},{where: {id:req.params.id}})
+        return res.status(200).json({
+            ok:true,
+            data:[movies]
+        })
+    }catch(e){
+        return res.status(500).json({error: 'Error en el servidor'})
+    }
+}
+
+const deleteMovie = async(req, res=response) =>{
+    try{
+        const movie = await Movie.deleteOne({where:{id: req.params.id}});
+        return res.json({
+            ok:true,
+            status: 201
+        })
+
+    }catch(e){
+        return res.status(500).json({
+            ok:false,
+            message: 'Error en el servidor'
+        })
+    }
 }
 
 const getRandomMovie = async (req, res = response) => {
-
+    
 }
 
 module.exports = {
-    getAllMovies, getRandomMovie, createMovie
+    getAllMovies, getRandomMovie, createMovie, updateMovie, deleteMovie
 }
--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -1,9 +1,14 @@
 const { Router } = require('express');
-const { getAllMovies, getRandomMovie} =require('../controllers/movies.controllers');
+const { getAllMovies, getRandomMovie, createMovie, updateMovie, deleteMovie} =require('../controllers/movies.controllers');
 
 const router= Router();
 
 router.get('/', getAllMovies); //http://localhost:3000/api/v1/movies GET
-router.get('/randomMovie', getRandomMovie);
+router.get('/randomMovie', getRandomMovie); //http://localhost:3000/api/v1/movies/randomMovie GET
+router.post('/',createMovie); //http://localhost:3000/api/v1/movies POST
+router.put('/:id/edit',updateMovie); //http://localhost:3000/api/v1/movies/edit/:id POST
+router.delete('/delete/:id',deleteMovie); //http://localhost:3000/api/v1/movies/delete/:id DELETE
+
+
 
 module.exports = router;
