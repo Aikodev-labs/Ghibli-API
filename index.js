d56a22e75d8c0ef5560e797ad2ce9772344b7180
Josué Oroya <47667368+joshuacba08@users.noreply.github.com>
2023-07-01 02:19:41 +0000

    error correction

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 
-const { db } = require('./src/db/config.js');
+const { db } = require('./src/db/config');
 db();
 
-const { App } = require('./src/app.js');
+const { App } = require('./src/app');
 
 const server = new App();
 
